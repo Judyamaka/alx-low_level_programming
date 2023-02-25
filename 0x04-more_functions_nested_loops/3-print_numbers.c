63f5ab9ffb2683078a8fb42ff07dc6bb41b1aaee
Judyamaka <amakaegonu100@gmail.com>
2023-02-24 20:04:20 -0800

    tasks C more functions, nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print digit (0 through 9)
+ *
+ * Return: Always 0
+**/
+void print_numbers(void)
+{
+
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
